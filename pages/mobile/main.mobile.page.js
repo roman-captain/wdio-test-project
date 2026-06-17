bbe0bc42b9335949ec7143badad9000988778ffd
Roman Horodnichev <romangorodnichev@gmail.com>
2026-06-17 12:44:38 +0200

    fix: update selectors broken by GitHub UI changes
    - subscribeBtn: href changed from absolute to relative (/newsletter)
      scoped to section[aria-labelledby="subscribe-to-newsletter"] for stability
    - privacyCheckbox: replaced aria-hidden SVG selector with real input
      [name="marketing_email_opt_in"]; click via browser.execute() because
      input is CSS-hidden (styled checkbox pattern)

--- pages/mobile/main.mobile.page.js
+++ pages/mobile/main.mobile.page.js
@@ -3,11 +3,11 @@ import BaseMobilePage from './base.mobile.page.js'
 class MainMobilePage extends BaseMobilePage {
 
     // Subscribe locators
-    get subscribeBtn()        { return $('a[href="https://github.com/newsletter"]') }
+    get subscribeBtn()        { return $('section[aria-labelledby="subscribe-to-newsletter"] a[href="/newsletter"]') }
     get subscribeHeadTitle()  { return $('h1=Get our developer newsletter') }
     get emailField()          { return $('#form-field-emailAddress') }
     get countrySelect()       { return $('#form-field-country') }
-    get privacyCheckbox()     { return $('[viewBox="0 0 100 100"]') }
+    get privacyCheckbox()     { return $('[name="marketing_email_opt_in"]') }
     get successTitle()        { return $('#hero-section-brand-heading') }
     get finalSubscribeBtn()   { return $('[data-analytics-event*="form_submit"]') }
 
@@ -45,8 +45,8 @@ class MainMobilePage extends BaseMobilePage {
     }
 
     async clickPrivacyCheckbox() {
-        await this.privacyCheckbox.waitForClickable({ timeout: 10000 })
-        await this.privacyCheckbox.click()
+        await this.privacyCheckbox.waitForExist({ timeout: 10000 })
+        await browser.execute(el => el.click(), await this.privacyCheckbox)
     }
 
     async clickFinalSubscribeBtn() {
